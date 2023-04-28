552b6fffae27e2b5f5edc21daf4796a9bcf77289
Keil-ENZO <enzo.keil06@icloud.com>
2023-04-28 09:28:17 +0200

    correction des consignes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form");
 
-class taches {
+class Tasks {
   constructor(id, title, completed) {
     this.id = id;
     this.title = title;
@@ -8,48 +8,98 @@ class taches {
   }
 }
 
-let tache = [
-  new taches(1, "Faire les courses", false),
-  new taches(2, "Faire le ménage", false),
-];
+let taskList = {
+  tasks: [
+    {
+      id: 1,
+      title: "Faire les courses",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Faire la lessive",
+      completed: true,
+    },
+    {
+      id: 3,
+      title: "Faire du sport",
+      completed: false,
+    },
+  ],
+};
 
+// Affichage de la liste des tâches
+function displayTasks() {
+  const listElement = document.getElementById("list");
+  listElement.innerHTML = "";
 
-//Display tasks
-function displayTaches() {
-  tache.forEach((tache) => {
-    list.innerHTML += `<li data-completed=${tache.completed} data-id=${tache.id}>${tache.title}</li>`;
+  taskList.tasks.forEach((task) => {
+    const taskItem = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("class", "checkbox");
+    checkbox.type = "checkbox";
+    checkbox.checked = task.completed;
+    checkbox.addEventListener("change", () => {
+      markTaskAsCompleted(task.id);
+    });
+
+    const taskTitle = document.createElement("span");
+    taskTitle.textContent = task.title;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = `<img src="./assets/delete_forever_FILL0_wght400_GRAD0_opsz48.svg" alt="">`;
+    deleteButton.addEventListener("click", () => {
+      deleteTask(task.id);
+    });
+
+    taskItem.appendChild(checkbox);
+    taskItem.appendChild(taskTitle);
+    taskItem.appendChild(deleteButton);
+    listElement.appendChild(taskItem);
   });
 }
 
-const local = window.localStorage.getItem("tache");
-displayTaches(local);
+// Ajouter une tâche à la liste
+function addTask() {
+  const taskInput = document.getElementById("item");
+  const taskTitle = taskInput.value.trim();
 
-//Add tasks
-function Addtasks() {
-    let newTache = new taches(tache.length + 1, item.value, false);
-    tache.push(newTache);
-    list.innerHTML += `<li data-completed=${tache.completed} data-id=${tache.id}>${item.value}</li>`;
-    item.value = "";
+  if (taskTitle !== "") {
+    const newTask = {
+      id: taskList.tasks.length + 1,
+      title: taskTitle,
+      completed: false,
+    };
 
-    window.localStorage.setItem("tache", JSON.stringify(tache));
+    taskList.tasks.push(newTask);
+    displayTasks();
+    taskInput.value = "";
+  }
 }
 
-//Delete and completed tasks
-function deleteCompletedTasks(e) {
-    if (e.target.dataset.completed === "true") {
-      e.target.remove();
-      e.target.classList.remove("checked");
-      tache.splice(e.target.dataset.id - 1, 1);
-      return;
-    } else {
-      e.target.dataset.completed = true;
-      e.target.classList.add("checked");
-    }
+// Marquer une tâche comme terminée
+function markTaskAsCompleted(taskId) {
+  const task = taskList.tasks.find((task) => task.id === taskId);
+
+  if (task) {
+    task.completed = !task.completed;
+    displayTasks();
   }
+}
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  Addtasks();
+// Supprimer une tâche de la liste
+function deleteTask(taskId) {
+  const taskIndex = taskList.tasks.findIndex((task) => task.id === taskId);
+
+  if (taskIndex !== -1) {
+    taskList.tasks.splice(taskIndex, 1);
+    displayTasks();
+  }
+}
+
+window.addEventListener("load", displayTasks);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addTask();
 });
-
-list.addEventListener("click", deleteCompletedTasks)
